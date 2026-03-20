1eaaaaf981ae76446e73e22fd4686202033866a3
mahesh-ganji9 <mahesh9.storage@gmail.com>
2026-03-20 17:49:35 +0530

    update terraform code

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  # vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.ami_data.id
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
